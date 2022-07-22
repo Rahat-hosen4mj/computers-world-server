b368efb6ccdd76826a86b2de766a50ceba9c844b
Your Name <you@example.com>
2022-07-23 00:58:22 +0600

    order guli show korte parci

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ox1ut.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("computer_world").collection("parts");
+    const orderCollection = client.db("computer_world").collection("orders");
 
-async function run(){
-    try{
-        await client.connect();
-        const partsCollection = client.db('computer_world').collection('parts');
-        const orderCollection = client.db('computer_world').collection('orders');
+    // get part
+    app.get("/part", async (req, res) => {
+      const query = {};
+      const cursor = partsCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts);
+    });
 
-       
-      // get part
-        app.get('/part', async(req, res) =>{
-            const query = {};
-            const cursor = partsCollection.find(query);
-            const parts = await cursor.toArray();
-            res.send(parts);
-        })
+    app.get("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const part = await partsCollection.findOne(query);
+      res.send(part);
+    });
 
-        app.get('/part/:id', async(req, res) =>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)};
-          const part = await partsCollection.findOne(query);
-          res.send(part)
-      });
+    // get order from server
+    app.get("/order", async (req, res) => {
+      const buyer = req.query.buyer || "nusrat@gmail.com";
+      const query = { buyer: buyer };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
 
-      // post order in the server
-      app.post('/order', async (req, res) => {
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send( result );
-      })
-
-
-    }
-    finally{
-
-    }
+    // post order in the server
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      // const query = { product: order.product,  buyer: order.buyer }
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Computers wrold server side running')
-})
+app.get("/", (req, res) => {
+  res.send("Computers wrold server side running");
+});
 
 app.listen(port, () => {
-  console.log(`Computers wrold server side running : ${port}`)
-})
+  console.log(`Computers wrold server side running : ${port}`);
+});
